75d8c8d3698480b3fad1a8197348d137f0c0a980
Abhijeet Kumar <abhi42cy7@gmail.com>
2025-08-22 19:12:50 +0530

    whole stack changed 2

--- src/components/Pipeline.jsx
+++ src/components/Pipeline.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect, useRef } from 'react'
 import gsap from 'gsap'
-import './Pipeline.css'
+import './Pipelinep.css'
 import { motion, AnimatePresence } from 'framer-motion'
 
 const Pipeline = () => {
